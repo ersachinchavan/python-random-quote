88b1501023096011109ad60976b1f4288322976d
ersachinchavan <er.sachinchavan@gmail.com>
2020-05-20 11:13:08 +0000

    Hellow world

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,11 @@
 def main():
   print("Keep it logically awesome.")
-
+  import random
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-
-  print(quotes[13])
+  last = len(quotes) - 1
+  print(quotes[rnd])
 
 if __name__== "__main__":
   main()
